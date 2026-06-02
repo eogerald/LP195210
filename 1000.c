7c47d2386c9ea7f25c8141007ae45982bbc898b8
eogerald <jefferson.gerald@unifesp.br>
2026-06-02 16:25:34 -0300

    Create 1000.c to print 'Hello World'
    Initial implementation of a C program to print 'Hello World'.

---
+++ 1000.c
@@ -0,0 +1,16 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Jefferson Gerald Pinto
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1071
+Data        : 24/03/2026
+Objetivo    : Ler dois valores inteiros e mostrar a soma.
+Aprendizado : funcionamento da logica de programacao.
+Pergunta de segunda ordem: <<<Fazer a pergunta>>>
+-------------------------------------------------------------------------- */
+#include <stdio.h>
+
+int main() {
+    printf("Hello World!\n");
+    return 0;
+}
