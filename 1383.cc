66e5f88a48713cde0664f6b2f112e15c8b72af9d
eogerald <jefferson.gerald@unifesp.br>
2026-06-11 14:41:53 -0300

    Create 1383.cc

---
+++ 1383.cc
@@ -0,0 +1,77 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : jefferson gerald
+Linguagem   : C++
+Problema    : https://judge.beecrowd.com/pt/problems/view/1071
+Data        : 111/06/2026
+Objetivo    : Ler dois valores inteiros e mostrar a soma.
+Aprendizado : <<<Descrever o que foi aprendido com este exercício>>>
+Pergunta de segunda ordem: <<<Fazer a pergunta>>>
+-------------------------------------------------------------------------- */
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int sudoku[9][9];
+
+bool verificaLinha(int x){
+    set<int> numeros;
+
+    for(int i = 0; i < 9; ++i){
+        numeros.insert(sudoku[x][i]);
+    }
+
+    return numeros.size() == 9;
+}
+
+bool verificaColuna(int x){
+    set<int> numeros;
+
+    for(int i = 0; i < 9; ++i){
+        numeros.insert(sudoku[i][x]);
+    }
+
+    return numeros.size() == 9;
+}
+
+bool verificaQuadrado(int x){
+    set<int> numeros;
+    int linha = 3*(x/3), coluna = 3*(x%3);
+
+    for(int i = 0; i < 3; ++i){
+        for(int j = 0; j < 3; ++j){
+            numeros.insert(sudoku[linha + i][coluna + j]);
+        }
+    }
+
+    return numeros.size() == 9;
+}
+
+int main(){
+    int n;
+    string resposta;
+
+    cin >> n;
+    for(int k = 1; k <= n; ++k){
+        for(int i = 0; i < 9; ++i){
+            for(int j = 0; j < 9; ++j){
+                cin >> sudoku[i][j];
+            }
+        }
+
+        cout << "Instancia " << k << endl;
+
+        resposta = "SIM";
+        for(int i = 0; i < 9; ++i){
+            if(!verificaLinha(i) || !verificaColuna(i) || !verificaQuadrado(i)){
+                resposta = "NAO";
+                break;
+            }
+        }
+
+        cout << resposta << endl << endl;
+    }
+
+    return 0;
+}
